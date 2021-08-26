cab5689835e093015fcfcdebe6147266b05b0310
Nayoung <nybae2003@naver.com>
2021-08-27 01:04:44 +0900

    sort update

--- week_3/03_insertion_sort.py
+++ week_3/03_insertion_sort.py
@@ -1,8 +1,17 @@
+# https://nazero.tistory.com/79
+# 다음과 같이 숫자로 이루어진 배열이 있을 때, 오름차순으로 삽입 정렬을 이용해서 정렬하시오.
+
 input = [4, 6, 2, 9, 1]
 
 
 def insertion_sort(array):
-    # 이 부분을 채워보세요!
+    n = len(array)
+    for i in range(1, n):
+        for j in range(i):
+            if array[i - j - 1] > array[i - j]:
+                array[i - j - 1], array[i - j] = array[i - j], array[i - j - 1]
+            else:
+                break
     return
 
 
