f578273406d3449da857ab4a631c94e6e9b7db54
Nayoung <nybae2003@naver.com>
2021-08-28 00:06:27 +0900

    merge sort update

--- week_3/04_merge.py
+++ week_3/04_merge.py
@@ -1,4 +1,4 @@
-array_a = [1, 2, 3, 4, 5]
+array_a = [1, 2, 3, 5]
 array_b = [4, 6, 7, 8]
 
 
@@ -14,12 +14,12 @@ def merge(array1, array2):
             result.append(array2[array2_index])
             array2_index += 1
 
-    if array1_index == len(array1):
+    if array1_index == len(array1):     # array2가 남아있다
         while array2_index < len(array2):
             result.append(array2[array2_index])
             array2_index += 1
 
-    if array2_index == len(array2):
+    if array2_index == len(array2):     # array1이 남아있다
         while array1_index < len(array1):
             result.append(array1[array1_index])
             array1_index += 1
--- week_3/04_merge_nayoung.py
+++ week_3/04_merge_nayoung.py
@@ -15,10 +15,12 @@ def merge(array1, array2):
         else:
             merge_array.append(array2[array2_index])
             array2_index += 1
+    # array2가 merge_array에 append를 모두 했음에도 불구하고 array1의 값들이 남아있는 상황
     if array1_index > array1_len-1:
         while array2_index <= array2_len-1:
             merge_array.append(array2[array2_index])
             array2_index += 1
+    # array1가 merge_array에 append를 모두 했음에도 불구하고 array2의 값들이 남아있는 상황
     if array2_index > array2_len-1:
         while array1_index <= array1_len-1:
             merge_array.append(array1[array1_index])
