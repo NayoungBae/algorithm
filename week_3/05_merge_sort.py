a621f7161ab54d27bbcb013903c805836903a274
Nayoung <nybae2003@naver.com>
2021-08-28 01:27:52 +0900

    merge sort update

--- week_3/05_merge_sort.py
+++ week_3/05_merge_sort.py
@@ -1,8 +1,20 @@
 # https://nazero.tistory.com/81
-# 두 array를 원소 순서대로 정렬하여 하나의 array로 합치기
+# 병합정렬
 
-array_a = [1, 2, 3, 5]
-array_b = [4, 6, 7, 8]
+array = [5, 3, 2, 1, 6, 8, 7, 4]
+
+
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left_array = merge_sort(array[:mid])
+    right_array = merge_sort(array[mid:])
+    # print(array)
+    # print("left_array: ", left_array)
+    # print("right_array: ", right_array)
+    # print()
+    return merge(left_array, right_array)
 
 
 def merge(array1, array2):
@@ -17,12 +29,12 @@ def merge(array1, array2):
             result.append(array2[array2_index])
             array2_index += 1
 
-    if array1_index == len(array1):     # array2가 남아있다
+    if array1_index == len(array1):
         while array2_index < len(array2):
             result.append(array2[array2_index])
             array2_index += 1
 
-    if array2_index == len(array2):     # array1이 남아있다
+    if array2_index == len(array2):
         while array1_index < len(array1):
             result.append(array1[array1_index])
             array1_index += 1
@@ -30,4 +42,4 @@ def merge(array1, array2):
     return result
 
 
-print(merge(array_a, array_b))  # [1, 2, 3, 4, 5, 6, 7, 8] 가 되어야 합니다!
+print(merge_sort(array))  # [1, 2, 3, 4, 5, 6, 7, 8]
--- week_3/05_merge_sort_nayoung.py
+++ week_3/05_merge_sort_nayoung.py
@@ -1,8 +1,16 @@
 # https://nazero.tistory.com/81
-# 두 array를 원소 순서대로 정렬하여 하나의 array로 합치기
+# 병합정렬
 
-array_a = [1, 2, 3, 4, 5]
-array_b = [4, 6, 7, 8]
+array = [5, 3, 2, 1, 6, 8, 7, 4]
+
+
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    array_len_half = len(array) // 2
+    merge_left = merge_sort(array[:array_len_half])
+    merge_right = merge_sort(array[array_len_half:])
+    return merge(merge_left, merge_right)
 
 
 def merge(array1, array2):
@@ -11,8 +19,7 @@ def merge(array1, array2):
     array2_index = 0
     array1_len = len(array1)
     array2_len = len(array2)
-    # array1과 array2 둘 중 하나가 끝 인덱스를 넘어갈 때까지 반복
-    while array1_index <= array1_len-1 and array2_index <= array2_len-1:
+    while array1_index <= array1_len - 1 and array2_index <= array2_len - 1:
         if array1[array1_index] < array2[array2_index]:
             merge_array.append(array1[array1_index])
             array1_index += 1
@@ -20,16 +27,16 @@ def merge(array1, array2):
             merge_array.append(array2[array2_index])
             array2_index += 1
     # array2가 merge_array에 append를 모두 했음에도 불구하고 array1의 값들이 남아있는 상황
-    if array1_index > array1_len-1:
-        while array2_index <= array2_len-1:
+    if array1_index > array1_len - 1:
+        while array2_index <= array2_len - 1:
             merge_array.append(array2[array2_index])
             array2_index += 1
     # array1가 merge_array에 append를 모두 했음에도 불구하고 array2의 값들이 남아있는 상황
-    if array2_index > array2_len-1:
-        while array1_index <= array1_len-1:
+    if array2_index > array2_len - 1:
+        while array1_index <= array1_len - 1:
             merge_array.append(array1[array1_index])
             array1_index += 1
     return merge_array
 
 
-print(merge(array_a, array_b))  # [1, 2, 3, 4, 5, 6, 7, 8]
+print(merge_sort(array))  # [1, 2, 3, 4, 5, 6, 7, 8]
